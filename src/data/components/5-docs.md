7e4813a88c9eb45bc5017c21a5fe631c6b196924
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-04-02 11:17:01 +0200

    rework

---
+++ src/data/components/5-docs.md
@@ -0,0 +1,12 @@
+---
+title: Geo Engine Docs
+subtitle: docs
+icon: ../../images/GeoEngine_Bildmarke_Schutzraum.svg
+links:
+    - title: View Documentation
+      url: https://docs.geoengine.io
+    - title: GitHub
+      url: https://github.com/geo-engine/docs
+---
+
+Geo Engine Docs provides the official documentation for the Geo Engine project.
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/preline/preline.js",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
-        ],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
+  // content: [
+  //   "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  //   "./node_modules/preline/preline.js",
+  // ],
+  // darkMode: "class",
+  // darkMode: 'false',
+  // theme: {
+  //   extend: {
+  //     fontFamily: {
+  //       sans: [
+  //         "Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+  //       ],
+  //     },
+  //   },
+  // },
+  // plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
+// disabled dark mode
+// localStorage.theme = 'light';
