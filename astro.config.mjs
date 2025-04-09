345e40af99f48cb8d4d9153ec555f3e20ed695cf
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-04-09 14:25:54 +0200

    other nav items

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@
 import {defineConfig} from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.geoengine.io',
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    experimental: {
-        svg: true,
-    },
+  site: 'https://www.geoengine.io',
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  experimental: {
+      svg: true,
+  },
+
+  integrations: [icon()],
 });
